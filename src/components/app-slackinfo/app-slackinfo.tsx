e9e2bf7c0149fdd9fbc2d5310a82013d6ee76ab3
Thomas Hochörtler <hochitom@me.com>
2019-10-01 12:06:01 +0200

    replaced link with invite url

--- src/components/app-slackinfo/app-slackinfo.tsx
+++ src/components/app-slackinfo/app-slackinfo.tsx
@@ -10,7 +10,7 @@ export class AppSlackinfo {
       <div class="app-slackinfo">
         <div class="content__slackinfo wrapper--base">
           <h2>Join us on</h2>
-          <a href="http://angularaustria.slack.com">
+          <a href="https://angularaustria.slack.com/join/shared_invite/enQtNzQ0OTMwNjkwODcwLWMwNDg3ZTQ2NWNjODA5Y2VjMjMwYTgwNzliN2MwNGRkNjA1NjcwNDg1ZmE3MzhjNDIxOTY5NDM2YzhlYWE2ZmQ">
             <img src="../../assets/imgs/icon-slack_full.svg" alt="Full Slack Icon" />
           </a>
           <p>Join our slack channel to unlock the full community power!</p>
